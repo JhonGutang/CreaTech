b6e03d6a18d92ade645acb30a668df2c5f8ff696
Jhon Barry Gutang <jhonbarrygutang@gmail.com>
2024-07-18 16:59:22 +0800

    adding authentication using vueitfy

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,17 @@
 import './bootstrap';
 import { createApp } from 'vue';
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
 import router from "./router/router";
 
 import App from "./App.vue"
 import Auth from "./Auth.vue"
 
-const vuetify = createVuetify({
-    components,
-    directives,
-})
 
+<<<<<<< HEAD
 createApp(Auth).use(router).use(vuetify).mount("#auth")
 createApp(App).use(router).mount("#app")
+=======
+createApp(App).use(router).mount("#app")
+createApp(Auth).use(router).mount("#auth")
+>>>>>>> main
