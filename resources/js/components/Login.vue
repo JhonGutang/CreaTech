17af1aa6d511d77d065e37d9ecc4f0b6cd4e87b0
Jhon Barry Gutang <jhonbarrygutang@gmail.com>
2024-07-18 12:00:55 +0800

    experimenting vuetify

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,10 +1,20 @@
 <template>
-  <v-app class="">
-    <v-main>
-      <v-container class="border-primary-thin">
-        <h1 class="border-primary-lg">Hello</h1>
-      </v-container>
-    </v-main>
+  <v-app>
+    <v-container class="container w-75 h-75 my-auto" >
+      <v-row class="h-100">
+        <v-col cols="6" class="border-lg h-100">
+          <v-sheet>
+            <v-btn color="success">text</v-btn>
+          </v-sheet>
+        </v-col>
+        <v-col class="border-lg">
+          <v-sheet>
+            <v-btn color="success">text</v-btn>
+
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
@@ -14,6 +24,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .container {
+    border: 3px solid red;
+  }
 </style>
